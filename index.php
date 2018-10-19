bf17fb2680a4f6125c0d70684fda58f742b3cbb3
Conner Gieg <connergieg@hotmail.com>
2018-10-19 16:08:50 +0000

    added order by team or name inputs, checkbox to show teams, and user feedback to add form submit

--- index.php
+++ index.php
@@ -33,13 +33,34 @@
             } else {
                 $playerName = "";
             }
-            header("Location: index.php?playerName=$playerName&catId=$catId&submit=".$_GET["submit"]);
+            $location = "Location: index.php?id=".$_POST["itemId"]."&playerName=$playerName&catId=$catId&submit=".$_GET["submit"];
+            if (isset($_GET["orderBy"])) {
+                $orderBy = $_GET["orderBy"];
+                $location .= "&orderBy=$orderBy";
+            }
+            if (isset($_GET["team"])) {
+                $team = $_GET["team"];
+                $location .= "&team=$team";
+            }
+            header($location);
         }
     }
     
     function selectCategory($catId) {
         if ($_GET["catId"] == $catId) {
             return "selected";
+        }
+    }
+    
+    function checkRadio($orderBy) {
+        if ($_GET["orderBy"] == $orderBy) {
+            echo "checked";
+        }
+    }
+    
+    function checkCheckBox() {
+        if ($_GET["team"] == "on") {
+            echo "checked";
         }
     }
     
@@ -88,6 +109,7 @@
         $playerName = $_GET["playerName"];
         $catId = $_GET["catId"];
         $newCatId = getCategoryId();
+        $orderBy = $_GET["orderBy"];
         
         if (!empty($playerName)) {
             $sql = "SELECT * FROM sports_players
@@ -102,10 +124,16 @@
             if (!empty($catId)) {
                 $sql .= " AND catId = $catId";
             }
+            if (isset($orderBy)) {
+                $sql .= " ORDER BY $orderBy";
+            }
         } else {
             if (!empty($catId)) {
                 $sql .= "SELECT * FROM sports_players
                     WHERE catId = $catId";
+                if (isset($orderBy)) {
+                    $sql .= " ORDER BY $orderBy";
+                }
             }
         }
         if (!empty($sql)) {
@@ -122,16 +150,29 @@
                     $itemCatId = $record["catId"];
                     $itemName = $record["playerName"];
                     $itemImage = $record["playerImage"];
+                    $itemTeam = $record["playerTeam"];
                     $itemPrice = $record["price"];
                     echo "<tr>";
                     echo "<td><a href='playerInfo.php?name=$itemName&image=".$itemImage."'>".$itemName."</a></td>";
+                    if (isset($_GET["team"])) {
+                        if (!empty($itemTeam)) {
+                            echo "<td>$itemTeam</td>";
+                        }
+                    }
+                    if ($_GET["id"] == $itemId) {
+                        $class = "added";
+                        $value = "Added";
+                    } else {
+                        $class = "";
+                        $value = "Add";
+                    }
                     echo "<td><form id='add' method='POST'>
                     <input type='hidden' name='itemId' value='$itemId'>
                     <input type='hidden' name='itemCatId' value='$itemCatId'>
                     <input type='hidden' name='itemName' value='$itemName'>
                     <input type='hidden' name='itemImage' value='$itemImage'>
                     <input type='hidden' name='itemPrice' value='$itemPrice'>
-                    <input type='submit' value='Add'>
+                    <input class='$class' type='submit' value='$value'>
                     </form></td>";
                     echo "</tr>";
                 }
@@ -154,9 +195,16 @@
             <select name="catId">
                 <option value="">- None -</option>
                 <?= displayCategories() ?>
-            </select>
+            </select> <br>
+            Order Results By: 
+            <input type="radio" id="teamOrd" name="orderBy" value="playerTeam" <?=checkRadio("playerTeam")?>>
+            <label for="teamOrd">Player Team</label>
+            <input type="radio" id="nameOrd" name="orderBy" value="playerName" <?=checkRadio("playerName")?>>
+            <label for="nameOrd">Player Name</label><br>
+            <label for="team">Display player's team</label>
+            <input type="checkbox" id="team" name="team" <?=checkCheckBox()?>><br><br>
             <input type="submit" name="submit" value="Submit">
-        </form> <br>
+        </form><br>
         
         <table>
         <?php
