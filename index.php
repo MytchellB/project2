b5ac30b8e7f1c31c889d821336270c628706f031
Mytchell Beaton <mbeaton@csumb.edu>
2018-11-05 20:31:32 +0000

    added bootstrap for styling

--- index.php
+++ index.php
@@ -225,7 +225,9 @@
 <html>
     <head>
         <title>Sports Cards</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" type="text/css" />
         <link rel="stylesheet" href="css/styles.css" type="text/css" />
+
     </head>
     <body>
         <main>
@@ -327,5 +329,7 @@
             </table>
             </div>
         </main>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </body>
 </html>
